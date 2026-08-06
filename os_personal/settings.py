870ef19e6b01f9763d3d742391fe1b0ff2dd7b65
samueldav360 <samueldav360@gmail.com>
2026-08-05 21:50:02 -0500

    Switch database configuration to PostgreSQL

--- os_personal/settings.py
+++ os_personal/settings.py
@@ -76,12 +76,11 @@ WSGI_APPLICATION = 'os_personal.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://neondb_owner:npg_9HUp1yDXwJrW@ep-soft-butterfly-ay0lv1yp.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require',
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
